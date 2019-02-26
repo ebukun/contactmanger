b7a06100f3dda9c79952e16d5fb30ebbdc6fbe72
ebukun <ibukunokunfolami@gmail.com>
2019-02-26 02:55:30 +0100

    Updates

---
+++ precache-manifest.f664d2518af3fb4efde33049178e18da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f92335250aa881191a97",
+    "url": "/contactmanger/static/js/runtime~main.f9233525.js"
+  },
+  {
+    "revision": "ce805d7c6aead79d9e0c",
+    "url": "/contactmanger/static/js/main.ce805d7c.chunk.js"
+  },
+  {
+    "revision": "9f685a0b81b09ea1d9d2",
+    "url": "/contactmanger/static/js/1.9f685a0b.chunk.js"
+  },
+  {
+    "revision": "ce805d7c6aead79d9e0c",
+    "url": "/contactmanger/static/css/main.7be0bb7c.chunk.css"
+  },
+  {
+    "revision": "9f685a0b81b09ea1d9d2",
+    "url": "/contactmanger/static/css/1.5703e61e.chunk.css"
+  },
+  {
+    "revision": "028965818817951076da6cd06868b1f4",
+    "url": "/contactmanger/index.html"
+  }
+];
